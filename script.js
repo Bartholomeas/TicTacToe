13f184bc70de9078555d0b5e5dfd0f374acb0a58
Bartosz <weivan321@gmail.com>
2021-05-15 14:52:11 +0200

    Finished Tic Tac Toe!

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const statusDisplay = document.querySelector('.game-status');
 
 let gameActive = true;
 let currentPlayer = 'X';
-const gameState = ['', '', '', '', '', '', '', '', '',];
+const gameState = ['', '', '', '', '', '', '', '', ''];
 let winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -22,47 +22,71 @@ statusDisplay.innerHTML = currPlayerTurn;
 
 
 const cellPlayed = (clickedCell, cellIndex) => {
+    // Ustawiam gdzie w gameState ma się pojawić symbol obecnego gracza
     gameState[cellIndex] = currentPlayer;
+    // Wstawia w miejsce klikniętej komórki symbol obecnego gracza
     clickedCell.innerHTML = currentPlayer;
 };
 
-const playerChange = () => { };
+const playerChange = () => {
+    statusDisplay.textContent = currPlayerTurn;
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+};
 
 const resultValid = () => {
     let roundWon = false;
+    /*Iteruje po gameState i kolejno do a, b, c przypisuje pozycje graczy i porównuje
+    je do kombinacji wygrywających*/
     for (i = 0; i <= 7; i++) {
         const winCondition = winningConditions[i];
         let a = gameState[winCondition[0]];
-        // console.log(a)
         let b = gameState[winCondition[1]];
-        // console.log(b)
         let c = gameState[winCondition[2]];
-        // console.log(c)
+
         if (a === '' || b === '' || c === '') continue;
+        // jeśli a, b, c są tego samego symbolu to runda się kończy
         if (a === b && b === c) {
             roundWon = true;
             break;
         }
     }
     console.log(gameState)
+    // jeśli roundWon = true to wyświetla komunikat o zwycięzcy i zwraca funkcje
     if (roundWon) {
         statusDisplay.textContent = winningMess;
+        gameActive = false;
+        return;
+    };
+
+    let roundDraw = !gameState.includes('');
+    if (roundDraw) {
+        statusDisplay.textContent = drawMess;
+        gameActive = false;
+        return;
     }
+
+    playerChange();
 };
 
 const cellClick = e => {
     const clickedCell = e.target;
+    // Przypisuje do zmiennej index data-cell-index
     const cellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
     if (gameState[cellIndex] !== '' || !gameActive) {
         return;
     }
-
     cellPlayed(clickedCell, cellIndex);
     resultValid();
 };
 
-const restartGame = () => { };
+const restartGame = () => {
+    gameActive = true;
+    currentPlayer = 'X';
+    const gameState = ['', '', '', '', '', '', '', '', ''];
+    statusDisplay.textContent = currPlayerTurn;
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = '');
+};
 
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', cellClick));
 document.querySelector('.restart-btn').addEventListener('click', restartGame);
