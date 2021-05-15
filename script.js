6777ca69a06ddbc516f74b9061221e8f9cfa83b7
Bartosz <weivan321@gmail.com>
2021-05-15 13:55:26 +0200

    Win conditions validator done

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const statusDisplay = document.querySelector('.game-status');
 
 let gameActive = true;
 let currentPlayer = 'X';
-let gameState = ['', '', '', '', '', '', '', '', ''];
+const gameState = ['', '', '', '', '', '', '', '', '',];
+let winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+];
 
 const winningMess = `Congratulations! ${currentPlayer} won!`;
 const drawMess = `It's a tie!`;
@@ -11,36 +21,46 @@ const currPlayerTurn = `It's ${currentPlayer} turn.`;
 statusDisplay.innerHTML = currPlayerTurn;
 
 
-const cellPlayed = () => { };
+const cellPlayed = (clickedCell, cellIndex) => {
+    gameState[cellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+};
 
 const playerChange = () => { };
 
-const resultValid = () => { };
+const resultValid = () => {
+    let roundWon = false;
+    for (i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        // console.log(a)
+        let b = gameState[winCondition[1]];
+        // console.log(b)
+        let c = gameState[winCondition[2]];
+        // console.log(c)
+        if (a === '' || b === '' || c === '') continue;
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+    console.log(gameState)
+    if (roundWon) {
+        statusDisplay.textContent = winningMess;
+    }
+};
 
 const cellClick = e => {
-
     const clickedCell = e.target;
     const cellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
-console.log(cellIndex)
+
     if (gameState[cellIndex] !== '' || !gameActive) {
         return;
     }
+
+    cellPlayed(clickedCell, cellIndex);
+    resultValid();
 };
-// const cellClick = e => {
-
-//     const clickedCell = e.target;
-//     const cellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
-
-// console.log(clickedCell)
-// console.log(cellIndex)
-
-//     if (gameState[cellIndex] !== '' || !gameActive) {
-//         return;
-//     }
-
-//     cellPlayed(clickedCell, cellIndex);
-//     resultValid();
-// };
 
 const restartGame = () => { };
 
